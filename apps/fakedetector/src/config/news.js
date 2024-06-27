09322f2c7ef9b196a1803432d5cac23ed852bd0e
Sonsoles López Pernas <poynting.slp@gmail.com>
2024-06-27 13:35:56 +0300

    Update articles

--- apps/fakedetector/src/config/news.js
+++ apps/fakedetector/src/config/news.js
@@ -6,32 +6,27 @@ export let NEWSen = [
     "feedback": ""
   },
   { "iframe":true,
-    "path": "https://sonsoles.me/balticsea2024/articles/poly.html",    
-    "comment": "Are these men refugees coming to England during the Syrian refugees crisis in 2015?",
+    "path": "https://sonsoles.me/balticsea2024/articles/gstm.html",    
     "true_or_false": false,
     "feedback": ""
   },
   { "iframe":true,
     "path": "https://sonsoles.me/balticsea2024/articles/comprehensive.html",    
-    "comment": "Are these men refugees coming to England during the Syrian refugees crisis in 2015?",
     "true_or_false": true,
     "feedback": ""
   },
   { "iframe":true,
     "path": "https://sonsoles.me/balticsea2024/articles/insecticide.html",    
-    "comment": "Are these men refugees coming to England during the Syrian refugees crisis in 2015?",
     "true_or_false": false,
     "feedback": ""
   },
   { "iframe":true,
     "path": "https://sonsoles.me/balticsea2024/articles/cherries.html",    
-    "comment": "Are these men refugees coming to England during the Syrian refugees crisis in 2015?",
     "true_or_false": true,
     "feedback": ""
   },
   { "iframe":true,
     "path": "https://sonsoles.me/balticsea2024/articles/unraveling.html",    
-    "comment": "Are these men refugees coming to England during the Syrian refugees crisis in 2015?",
     "true_or_false": false,
     "feedback": ""
   }
